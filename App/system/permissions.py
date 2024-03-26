6dd86b4e17f3185be0b9638b4729c93c188b89f3
mateusz <your@example.com>
2024-03-26 22:58:46 +0100

    update comment for permissions.py

--- App/system/permissions.py
+++ App/system/permissions.py
@@ -4,4 +4,10 @@ from rest_framework import permissions
 class Owner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
+        """
+        :param request: request
+        :param view: view
+        :param obj: obj
+        :return: boolean
+        """
         return obj.user == request.user
